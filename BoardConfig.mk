fdcc9a9fc938405742c4ce1c09ddd2a167416115
jerjjj <93431283+jerjjj@users.noreply.github.com>
2022-04-03 17:45:39 +0800

    add support
    add support

---
+++ BoardConfig.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/xiaomi/merlinx
+
+TARGET_BOOTLOADER_BOARD_NAME := merlinx
+
+# Inherit from mt6768-common
+-include device/xiaomi/mt6768-common/BoardConfigCommon.mk
+
+# Kernel
+TARGET_KERNEL_CONFIG := merlin_defconfig
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := merlin,merlinx,merlin_eea,merlinnfc
+
+# HIDL
+ODM_MANIFEST_SKUS += \
+    merlinnfc
+
+ODM_MANIFEST_MERLINNFC_FILES := $(DEVICE_PATH)/manifest_merlinnfc.xml
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
+
+# Audio
+USE_CUSTOM_AUDIO_POLICY := 1
+
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+BUILD_BROKEN_VINTF_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_PREBUILT_ELF_FILES := true
+SELINUX_IGNORE_NEVERALLOWS := true
